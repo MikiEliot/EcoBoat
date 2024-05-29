7e0532bc70c53456108ea021a713fe2a66bfa3a7
Miodrag Mihajlovic <lunatref@gmail.com>
2024-05-29 09:52:31 +0200

    repairs

--- API_ecoboat-main/src/db/capteur_magnetometre_db.js
+++
@@ -1,8 +0,0 @@
-const capteur_magnetometre = [{
-    x: 4,
-    y: 5,
-    z: 6,
-    created: new Date()
-}]
-
-module.exports = capteur_magnetometre
--- API_ecoboat-main/src/db/capteur_temperature_db.js
+++
@@ -1,8 +0,0 @@
-const capteur_temperature = [
-    {
-        temperature: 4,
-        created: new Date()
-    }
-];
-
-module.exports = capteur_temperature
